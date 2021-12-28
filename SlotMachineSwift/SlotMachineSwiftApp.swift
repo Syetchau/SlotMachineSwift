0d1bcb751a18350b8cddfb6916b5b0c0f2898746
syetchau95 <syetchau@hotmail.com>
2021-12-28 22:08:29 +0800

    Initial Commit

---
+++ SlotMachineSwift/SlotMachineSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineSwiftApp.swift
+//  SlotMachineSwift
+//
+//  Created by LiewSyetChau on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
